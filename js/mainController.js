07b3db3c20f837f42537c7058e4d3d4744bb25b1
dmayes403 <davidrmayes@gmail.com>
2017-02-11 13:04:59 -0700

    templates through searchFriends is done

---
+++ js/mainController.js
@@ -0,0 +1,14 @@
+angular.module('myApp')
+    .controller('mainCtrl', function($scope){
+      $scope.images = ['../images/2017-02-11_0001.jpg',
+      '../images/2017-02-11_0002.jpg',
+      '../images/2017-02-11_0003.jpg',
+      '../images/2017-02-11_0004.jpg',
+      '../images/2017-02-11_0005.jpg',
+      '../images/2017-02-11_0006.jpg',]
+
+      $scope.person = {
+        'image': '../images/2017-02-11_0010.jpg'
+      }
+
+    })
